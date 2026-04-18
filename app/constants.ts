45124dc47a278cd6e12c16f2e2eb09fb3cecb37d
Szymon <szymonjarosz102@gmail.com>
2026-04-18 19:37:32 +0200

    Added constants

--- app/constants.ts
+++ app/constants.ts
@@ -1,1 +1,3 @@
 export const transactionViewOptions = ['Yearly', 'Monthly', 'Daily']
+export const categories = ['Food', 'Housing', 'Car', 'Entertaiment']
+export const types = ['Income', 'Expense', 'Saving', 'Investment']
